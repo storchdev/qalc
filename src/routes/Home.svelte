ab749bb830c7034f2ca90b90a95f46c316436737
storchdev <pianister123@gmail.com>
2025-07-07 23:56:45 -0400

    template progress

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import Evaluator from "../lib/Evaluator";
+    import Evaluator from "../lib/evaluator";
     import { oneHanded } from "../lib/presets";
-    import ScientificNumber from "../lib/ScientificNumber";
+    import ScientificNumber from "../lib/scientificNumber";
+    import Icon from "@iconify/svelte";
 
     interface Keybinds {
         [key: string]: string;
+    }
+
+    interface Template {
+        name: string;
+        type: "expression" | "javascript";
+        parameters: string[];
+        content: string;
+    }
+
+    interface RawTemplate {
+        name: string | null;
+        type: "expression" | "javascript";
+        parameters: string | null;
+        content: string | null;
     }
 
     const openParenFollows: string[] = [
@@ -24,8 +39,43 @@
     let keybinds: Keybinds = {};
     let exprHistory: string[] = [];
     let ansHistory: number[] = [];
-    let maxHistoryLen: number;
-    let maxDigits = $state(6);
+    let maxHistoryLen: number = 50;
+    let maxDigits = $state(12);
+    let templates: Template[] = $state([]);
+    let showTemplates = $state(true); // change to false for real
+    let templateText = $derived(
+        showTemplates ? "Hide templates" : "Show templates",
+    );
+    let showAdd = $state(true); // change to false for real
+
+    let rawTemplate: RawTemplate = $state({
+        name: null,
+        type: "expression",
+        parameters: null,
+        content: null,
+    });
+    // let addTypeValue: string = $state("expression");
+
+    const javascriptContentPlaceholder = `// nCr example
+// only write the function body
+// ALWAYS return a number!
+
+if (r > n) return 0;
+if (r === 0 || r === n) return 1;
+
+r = Math.min(r, n - r);
+let result = 1;
+
+for (let i = 1; i <= r; i++) {
+    result *= (n - i + 1);
+    result /= i;
+}
+
+return result;
+`;
+    const javascriptParameterPlaceholder = "n,r";
+    const expressionContentPlaceholder = "(-{b}+sqrt({b}^2-4*{a}*{c}))/(2*{a})";
+    const expressionParameterPlaceholder = "a,b,c";
 
     let index = $state(0);
     let expr = $derived(exprHistory[index]);
@@ -34,7 +84,6 @@
     let numOpenParens: number = $state(0);
     let numCloseParens: number = $state(0);
     let molarMassMode: boolean = false;
-    // let keyStack: string[] = [];
 
     let showSettings: boolean = $state(false);
     let settingsText = $derived(showSettings ? "Back" : "Settings");
@@ -46,9 +95,14 @@
         keybinds = JSON.parse(localStorage.getItem("keybinds") ?? "{}");
         exprHistory = JSON.parse(localStorage.getItem("exprHistory") ?? "[]");
         ansHistory = JSON.parse(localStorage.getItem("ansHistory") ?? "[]");
-        maxHistoryLen = parseInt(localStorage.getItem("maxHistoryLen") ?? "50");
-        maxDigits = parseInt(localStorage.getItem("maxDigits") ?? "12");
+        maxHistoryLen = parseInt(
+            localStorage.getItem("maxHistoryLen") ?? maxHistoryLen.toString(),
+        );
+        maxDigits = parseInt(
+            localStorage.getItem("maxDigits") ?? maxDigits.toString(),
+        );
         index = ansHistory.length - 1;
+        templates = JSON.parse(localStorage.getItem("templates") ?? "[]");
     }
 
     // Load keybinds from localStorage on mount
@@ -255,7 +309,14 @@
         for (let i = 0; i < localStorage.length; i++) {
             const key = localStorage.key(i);
             if (key) {
-                if (["keybinds", "exprHistory", "ansHistory"].includes(key)) {
+                if (
+                    [
+                        "keybinds",
+                        "exprHistory",
+                        "ansHistory",
+                        "templates",
+                    ].includes(key)
+                ) {
                     data[key] = JSON.parse(localStorage.getItem(key) ?? "{}");
                 } else {
                     data[key] = JSON.parse(localStorage.getItem(key) ?? "{}");
@@ -398,9 +459,108 @@
                 rows="1"
             ></textarea>
             <p class="text-red-700 font-bold text-[0.5rem]">{errorLine}</p>
-        {/if}
-        <!-- <div>opening={numOpenParens}, closing={numCloseParens}</div> -->
-        {#if showSettings}
+            <div class="flex flex-col gap-1 w-1/2 text-[0.5rem]">
+                <button
+                    class="rounded border-2 p-2 hover:cursor-pointer w-full"
+                    onclick={() => {
+                        showTemplates = !showTemplates;
+                    }}>{templateText}</button
+                >
+                {#if showTemplates}
+                    <div
+                        class="flex flex-col gap-2 border-2 rounded p-2 w-full"
+                    >
+                        <button
+                            class="flex gap-1 items-center font-bold hover:cursor-pointer"
+                            onclick={() => {
+                                showAdd = !showAdd;
+                            }}
+                        >
+                            {#if showAdd}
+                                <Icon icon="fa:angle-down" />
+                            {:else}
+                                <Icon icon="fa:angle-right" />
+                            {/if}
+                            <p>Add</p>
+                        </button>
+                        {#if showAdd}
+                            <div>
+                                <p
+                                    class="font-bold border-rfont-bold border border-gray-300 rounded-t p-1 inline-block border-b-0"
+                                >
+                                    Name
+                                </p>
+                                <textarea
+                                    rows="1"
+                                    class="overflow-hidden resize-none border border-gray-300 rounded-b rounded-tr p-1 focus:outline-none focus:ring-2 w-full"
+                                    placeholder="my cool template"
+                                ></textarea>
+                            </div>
+                            <div>
+                                <p
+                                    class="font-bold border-rfont-bold border border-gray-300 rounded-t p-1 inline-block border-b-0"
+                                >
+                                    Type
+                                </p>
+                                <select
+                                    class="border border-gray-300 rounded-b rounded-tr p-1 focus:outline-none focus:ring-2 w-full"
+                                    bind:value={rawTemplate.type}
+                                >
+                                    <option value="expression"
+                                        >Expression</option
+                                    >
+                                    <option value="javascript"
+                                        >JavaScript</option
+                                    >
+                                </select>
+                            </div>
+                            <div>
+                                <p
+                                    class="font-bold border-rfont-bold border border-gray-300 rounded-t p-1 inline-block border-b-0"
+                                >
+                                    Parameters
+                                </p>
+                                <textarea
+                                    rows="1"
+                                    class="w-full overflow-hidden resize-none border border-gray-300 rounded-b rounded-tr p-1 focus:outline-none focus:ring-2"
+                                    placeholder={rawTemplate.type ===
+                                    "javascript"
+                                        ? javascriptParameterPlaceholder
+                                        : expressionParameterPlaceholder}
+                                ></textarea>
+                            </div>
+                            <div>
+                                <p
+                                    class="font-bold border-rfont-bold border border-gray-300 rounded-t p-1 inline-block border-b-0"
+                                >
+                                    {rawTemplate.type === "javascript"
+                                        ? "Code"
+                                        : "Expression"}
+                                </p>
+                                {#if rawTemplate.type === "javascript"}
+                                    <textarea
+                                        rows="10"
+                                        class="resize-none border border-gray-300 rounded-b rounded-tr p-1 focus:outline-none focus:ring-2 w-full"
+                                        placeholder={javascriptContentPlaceholder}
+                                    ></textarea>
+                                {:else}
+                                    <textarea
+                                        rows="1"
+                                        class="overflow-hidden resize-none border border-gray-300 rounded-b rounded-tr p-1 focus:outline-none focus:ring-2 w-full"
+                                        placeholder={expressionContentPlaceholder}
+                                    ></textarea>
+                                {/if}
+                            </div>
+                        {/if}
+                        {#each templates as template}
+                            <div class="p-2 border-2">
+                                {template.name}
+                            </div>
+                        {/each}
+                    </div>
+                {/if}
+            </div>
+        {:else}
             <div class="flex gap-4">
                 <button
                     class="rounded p-2 border-2 text-[0.5em] hover:cursor-pointer"
